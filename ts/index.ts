3142688569ce2f96047ff7daeeb30898a1d9d317
TheConnMan <bcconn2112@gmail.com>
2018-05-12 18:02:25 -0400

    Parse out the function name, version, and memory metrics

--- ts/index.ts
+++ ts/index.ts
@@ -10,8 +10,19 @@ exports.handler = (input: Lambda.CloudWatchLogsEvent, context: Lambda.Context) =
     if (e) {
       context.fail(e);
     } else {
-      result = JSON.parse(result.toString('ascii'));
-      console.log('Event Data:', JSON.stringify(result, null, 2));
+      const data: any = JSON.parse(result.toString('ascii'));
+      const functionName = data.logGroup.split('/').pop();
+      const version = /\[(.*)\]/g.exec(data.logStream)[1];
+      const metrics = data.logEvents.map((event) => {
+        const matches = /Memory Size: ([0-9].+) MB\tMax Memory Used: ([0-9].+)MB/g.exec(event.message);
+        return {
+          billed: parseInt(matches[1], 10),
+          used: parseInt(matches[2], 10)
+        };
+      });
+      console.log(`Function Name: ${functionName}`);
+      console.log(`Version: ${version}`);
+      console.log(`Metrics: ${JSON.stringify(metrics, null, 2)}`);
       context.done();
     }
   });
