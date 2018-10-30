0e0c3d61f6eaf2b570a116df54f78f84fac5bc10
TheConnMan <bcconn2112@gmail.com>
2018-10-30 11:48:39 -0400

    Add a basic test for the ParsingService
    Ref PHX-12358

---
+++ test/test.ts
@@ -0,0 +1,5 @@
+import utilizationService from '../ts/service/UtilizationService';
+
+// (async () => {
+//   await utilizationService.calculate(['utilization-dev-run'], 1540910111000 , 60000);
+// })();
