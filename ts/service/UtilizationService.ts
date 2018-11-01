84d942c8122d5cc021f64912568ec20a88fac8ed
TheConnMan <bcconn2112@gmail.com>
2018-11-01 12:40:32 -0400

    Add debug logs for the UtilizationService
    Ref PHX-12358

--- ts/service/UtilizationService.ts
+++ ts/service/UtilizationService.ts
@@ -13,10 +13,14 @@ export class UtilizationService {
       durationMs: number): Promise<void> {
     await Promise.all(functionNames.map(async (functionName) => {
       try {
+        logger.debug(`Starting collection for function ${functionName}`);
         const logs = await cloudWatchService.fetchLogs(functionName, endTime, durationMs, null);
+        logger.debug(`Collected ${logs.length} logs for function ${functionName}`);
         if (logs.length !== 0) {
           const samples = parsingService.parseLogs(logs);
+          logger.debug(`Parsed ${samples.length} samples for function ${functionName}`);
           await metriclyService.submitSamples(invokedArn, functionName, samples);
+          logger.debug(`Submitted samples for function ${functionName}`);
         }
       } catch (e) {
         logger.error('Error parsing and posting logs', e);
