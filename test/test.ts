7761c250e7e583e086ea2cca0f8bd79ef21e486b
TheConnMan <bcconn2112@gmail.com>
2018-11-01 12:26:28 -0400

    Remove accidentally committed file

--- test/test.ts
+++
@@ -1,5 +0,0 @@
-import utilizationService from '../ts/service/UtilizationService';
-
-// (async () => {
-//   await utilizationService.calculate(['utilization-dev-run'], 1540910111000 , 60000);
-// })();
