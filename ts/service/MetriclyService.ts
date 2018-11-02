1cd83397cee7b743eefcd8bc3578c0692d064aab
TheConnMan <bcconn2112@gmail.com>
2018-11-02 12:42:28 -0400

    Replace logged warnings and errors with thrown exceptions
    The Lambda function will now correctly be marked as a failure.
    
    Ref PHX-12358

--- ts/service/MetriclyService.ts
+++ ts/service/MetriclyService.ts
@@ -11,8 +11,7 @@ export class MetriclyService {
 
   public async submitSamples(arn: string, functionName: string, samples: Sample[]): Promise<void> {
     if (!MetriclyService.API_KEY) {
-      logger.warn(`The Metricly API key is not configured. Please add the METRICLY_API_KEY environment variable.`);
-      return;
+      throw new Error(`The Metricly API key is not configured. Please add the METRICLY_API_KEY environment variable.`);
     }
     const metrics: any[] = [...new Set(samples.map((sample) => sample.metric))].map((metricName) => {
       return {
@@ -39,7 +38,7 @@ export class MetriclyService {
     }).response;
     if (response.status !== 202) {
       const json = await response.json();
-      logger.error(`Error posting samples to Metricly (status ${response.status}): ${json.detail}`);
+      throw new Error(`Error posting samples to Metricly (status ${response.status}): ${json.detail}`);
     }
   }
 }
